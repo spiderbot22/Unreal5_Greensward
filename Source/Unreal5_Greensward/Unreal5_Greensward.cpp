825fbe9748357a3afe44d91bfdc53e9ac45c2658
Kristoff Pawinski <pawinskikristoff@gmail.com>
2024-08-07 11:47:02 -0700

    Created blank Unreal5 project

---
+++ Source/Unreal5_Greensward/Unreal5_Greensward.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Unreal5_Greensward.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Unreal5_Greensward, "Unreal5_Greensward" );
